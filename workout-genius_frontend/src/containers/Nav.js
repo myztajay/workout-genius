237a07023868ca9cfacdc4aaada71615b07f1537
myztajay <myztajay@hotmail.com>
2018-01-09 04:18:48 -0500

    Added Drawer to navbar as temporary alternative because navbar is not responsive atm

--- workout-genius_frontend/src/containers/Nav.js
+++ workout-genius_frontend/src/containers/Nav.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import { ToolbarGroup } from 'material-ui/Toolbar';
 import './nav.css'
 import FlatButton from 'material-ui/FlatButton'
@@ -17,6 +19,12 @@ const MyNavLinks = () => (
 );
 
 class Nav extends Component{
+  constructor(props) {
+  super(props);
+  this.state = {open: false};
+  }
+  handleToggle = () => this.setState({open: !this.state.open})
+  
   render(){
     return(
       <MuiThemeProvider>
@@ -24,8 +32,17 @@ class Nav extends Component{
         className='navbar'
         title={<span >Workout genius</span>}
         iconElementRight={<MyNavLinks />}
+        onClick={this.handleToggle}
         >
         </AppBar>
+        <Drawer 
+          open={this.state.open}
+          containerStyle={{backgroundColor:'#ecf0f1',}}
+        >
+          <NavLink to="/"><MenuItem>Home</MenuItem></NavLink>
+          <NavLink to="/workouts"><MenuItem>Workouts</MenuItem></NavLink>
+          <MenuItem>Menu Item 2</MenuItem>
+       </Drawer>
       </MuiThemeProvider>
     )
   }
