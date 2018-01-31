e7011ab6cbdbcfdc703cea64eef69954902cb1da
unknown <myztajay@hotmail.com>
2018-01-31 17:24:01 -0500

    Added partial functionality to filter function - still working

--- workout-genius_frontend/src/containers/WorkoutsContainer.js
+++ workout-genius_frontend/src/containers/WorkoutsContainer.js
@@ -9,13 +9,13 @@ import './workoutscontainer.css';
 
 class WorkoutsContainer extends Component{
   constructor(props){
-    super(props)  
+    super(props)
     this.state = {
       workouts: [],
       filterArr: []
     }
   }
-  
+
   componentDidMount(){
     axios.get('/api/workouts')
     .then((res)=>{
@@ -24,24 +24,57 @@ class WorkoutsContainer extends Component{
       })
     })
   }
-  
+
   onFilterToggle(type){
     if(this.state.filterArr.length === 0){
       this.setState({
           filterArr: [type]
       })
     }
-    let newArr = this.state.filterArr.filter((workoutType)=>{
-      if(workoutType != type){
-        return true
-      }
-      this.setState({
-          filterArr: [...this.state.filterArr, ...newArr]
-      })
+    let checkArr = this.state.filterArr.some((workoutType)=>{
+      return workoutType === type
     })
+    if(checkArr){
+      let newArr = this.state.filterArr.filter((workoutType)=>{
+        return workoutType != type
+      })
+      this.setState({
+        filterArr: [ ...newArr]
+      })
+    }else{
+      this.setState({
+        filterArr: [ type]
+      })
+    }
   }
-  
+
   renderWorkouts(){
+    if(this.state.filterArr.length > 0){
+      return this.state.workout.filter((workout)=>{
+        if(workout.workout_type === this.state.filterArr[0]){
+          return(
+            <Link to={'/workout/' + workout._id} >
+              <Card className='card-workout'>
+                <CardTitle titleColor="#2979FF"  />
+                <CardMedia
+                  overlay={<CardTitle title={workout.name} subtitle="" />}
+                >
+                <img className="workout-img" src="workout.jpeg" alt="" />
+                </CardMedia>
+                <div className="card-info">
+                  <div className="info"><h4>Exercise</h4><br /><p className="card-text">{workout.exercises.length}</p></div>
+                  <div className="info"><h4>Intensity</h4><br /><p className="card-text">{workout.intensity}</p></div>
+                  <div className="info"><h4>likes</h4><br /><p className="card-text">20</p></div>
+                </div>
+                <Divider />
+                <div className='user-info'><img className='profile-img' src={`${workout.creator[0].facebook_photo}`} /><p>{workout.creator[0].display_name}</p></div>
+              </Card>
+            </Link>
+          )
+      }})
+    }
+
+
     return this.state.workouts.map((workout)=>{
       return(
         <Link to={'/workout/' + workout._id} >
@@ -59,30 +92,28 @@ class WorkoutsContainer extends Component{
             </div>
             <Divider />
             <div className='user-info'><img className='profile-img' src={`${workout.creator[0].facebook_photo}`} /><p>{workout.creator[0].display_name}</p></div>
-            
           </Card>
         </Link>
       )
     })
   }
-  
+
   render(){
     return(
       <MuiThemeProvider>
       <div className="filter-div">
-        <WorkoutFilter name="Crossfit" abrev="CF" onFilterToggle={this.onFilterToggle.bind(this)} /> 
-        <WorkoutFilter name="Hybrid" abrev="H" onFilterToggle={this.onFilterToggle.bind(this)}  /> 
-        <WorkoutFilter name="BodyBuilding" abrev="BB" onFilterToggle={this.onFilterToggle.bind(this)} /> 
-        <WorkoutFilter name="Calisthetics" abrev="CS" onFilterToggle={this.onFilterToggle.bind(this)} /> 
-        <WorkoutFilter name="Cardio" abrev="CO" onFilterToggle={this.onFilterToggle.bind(this)} /> 
+        <WorkoutFilter name="Crossfit" abrev="CF" onFilterToggle={this.onFilterToggle.bind(this)} />
+        <WorkoutFilter name="Hybrid" abrev="H" onFilterToggle={this.onFilterToggle.bind(this)}  />
+        <WorkoutFilter name="BodyBuilding" abrev="BB" onFilterToggle={this.onFilterToggle.bind(this)} />
+        <WorkoutFilter name="Calisthetics" abrev="CS" onFilterToggle={this.onFilterToggle.bind(this)} />
+        <WorkoutFilter name="Cardio" abrev="CO" onFilterToggle={this.onFilterToggle.bind(this)} />
       </div>
-        <div className="main-container">            
+        <div className="main-container">
           {this.renderWorkouts()}
         </div>
       </MuiThemeProvider>
     )
   }
 }
-
 
 export default WorkoutsContainer
